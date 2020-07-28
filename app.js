2df91b1d7e39f820589b977dce3860d132b40d03
unknown <punithmatli@gmail.com>
2020-07-28 14:55:54 +0530

    Created a server using http createServer, sent POST request and got response

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const fs = require('fs');
+const http = require('http');
 
-const userName = 'Max';
+const server = http.createServer((req, res) => {
+    console.log('Incoming Request');
+    console.log(req.method, req.url);
 
-fs.writeFile('user-data.txt', 'Name: ' + userName, (err) => {
-  if (err) {
-    console.log(err);
-    return;
+  if(req.method === 'POST') {
+    let body = '';
+    req.on('end', () => {
+      const userName = body.split('=')[1];
+      res.end('<h1>' + userName + '</h1>')
+    })
+
+    req.on('data', (chunk) => {
+      body += chunk;
+    })
+  } else {
+    res.setHeader('Content-Type','text/html');
+    res.end('<form method="POST"><input type="text" name="username" /><button type="submit">Create User</button></form>');
   }
-  console.log('WROTE FILE');
-});
+
+})
+
+server.listen(5000)
