bba2b3a89f2ca2a3f3984473f4e141026e661d88
unknown <punithmatli@gmail.com>
2020-08-02 06:58:51 +0530

    Used body-parser package and modified routes

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    let body = '';
-    req.on('end', () => {
-        const username = body.split('=')[1];
-        if(username) req.body = { name: username };
-        next();
-    });
-    req.on('data', (chunk) => {
-        body += chunk;
-    })
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/user', (req, res, next) => {
+    res.send('<h1>Username:' + req.body.username + '</h1>');
 })
 
-app.use((req, res, next) => {
-    if (req.body) {
-        return res.send('<h1>' + req.body.name + '</h1>');
-    }
-    res.send('<form method="POST"><input type="text" name="username" /><button type="submit">Create User</button></form>')
+app.get('/', (req, res, next) => {
+    res.send('<form action="/user" method="POST"><input type="text" name="username" /><button type="submit">Create User</button></form>')
 })
 
 app.listen(5000);
